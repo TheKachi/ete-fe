5030ff23b5a3ea7461e95ef73f4a33f4380cee1b
Onyedikachi Nze-Ben <nzebenflorence@gmail.com>
2022-04-13 10:42:21 +0100

    convert service card into a component

---
+++ components/services/ServiceCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <nuxt-link :to="`/services/${service.id}`">
+    <div class="card h-full">
+      <div class="flex gap-x-[18px] items-center mb-24">
+        <span
+          class="rounded-[50%] h-[46px] w-[46px] leading-[46px] bg-[#D8DDFD] text-blue font-bold text-center"
+        >
+          {{ service.title.charAt(0).toUpperCase() }}
+        </span>
+
+        <h5 class="text-lg text-black font-medium">
+          {{ service.title }}
+        </h5>
+      </div>
+
+      <p class="text-base text-grey">
+        {{ slice(service.description) }}
+      </p>
+
+      <!-- <div class="grid grid-cols-6 float-right">
+                <div
+                  class="col-span-1 -mr-12 "
+                  v-for="(stakeH, i) in service"
+                  :key="i"
+                >
+                  <img :src="stakeH.userPhoto" :alt="meta" />
+                </div>
+                <div
+                  class="col-span-1 -mr-12 bg-transparent rounded-full h-24 w-24 text-black text-xs font-medium"
+                >
+                  +{{ service.stakeholders.length }}
+                </div>
+              </div> -->
+    </div>
+  </nuxt-link>
+  <!--  :to="{ name: 'id', params: { slug: service.id } }"  -->
+</template>
+
+<script>
+export default {
+  name: 'ServiceCard',
+  props: {
+    service: Object,
+  },
+  methods: {
+    slice(str) {
+      if (str.length <= 100) return str
+      return str.slice(0, 97) + '...'
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.card {
+  @apply bg-white p-20 pb-28 rounded-md shadow-[0_4px_50px_rgba(0,0,0,0.1)];
+}
+
+/* truncate {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  resize: horizontal;
+} */
+
+/* truncate-lg {
+  @apply text-lg
+width: 200px;
+  font-size: 1.3rem;
+} */
+/* truncate-sm {
+  @apply text-base;
+  width: 200px;
+  font-size: 1.3rem;
+} */
+</style>
--- pages/services/index.vue
+++ pages/services/index.vue
@@ -49,44 +49,7 @@
           v-if="allServices.length > 0"
         >
           <div v-for="service in allServices" :key="service.id">
-            <nuxt-link :to="`/services/${service.id}`">
-              <div class="card">
-                <div class="flex gap-x-[18px] items-center mb-24">
-                  <span
-                    class="rounded-[50%] h-[46px] w-[46px] leading-[46px] bg-[#D8DDFD] text-blue font-bold text-center"
-                  >
-                    {{ service.title.charAt(0).toUpperCase() }}
-                  </span>
-
-                  <h5 class="text-lg text-black font-medium">
-                    {{ service.title }}
-                  </h5>
-                </div>
-
-                <p class="text-base text-grey" v-if="service.description">
-                  {{ slice(service.description) }}
-                </p>
-
-                <p class="text-base text-grey" v-else>
-                  This service has no description.
-                </p>
-
-                <!-- <div class="grid grid-cols-6 float-right">
-                <div
-                  class="col-span-1 -mr-12 "
-                  v-for="(stakeH, i) in service"
-                  :key="i"
-                >
-                  <img :src="stakeH.userPhoto" :alt="meta" />
-                </div>
-                <div
-                  class="col-span-1 -mr-12 bg-transparent rounded-full h-24 w-24 text-black text-xs font-medium"
-                >
-                  +{{ service.stakeholders.length }}
-                </div>
-              </div> -->
-              </div>
-            </nuxt-link>
+            <service-card :service="service" />
           </div>
         </div>
 
@@ -104,42 +67,8 @@
           v-if="merchantServices.length > 0"
         >
           <div v-for="service in merchantServices" :key="service.id">
-            <nuxt-link :to="`/services/${service.id}`" class="">
-              <div class="card h-full">
-                <div class="flex gap-x-[18px] items-center mb-24">
-                  <span
-                    class="rounded-[50%] h-[46px] w-[46px] leading-[46px] bg-[#D8DDFD] text-blue font-bold text-center"
-                  >
-                    {{ service.title.charAt(0).toUpperCase() }}
-                  </span>
-
-                  <h5 class="text-black font-medium">
-                    {{ service.title }}
-                  </h5>
-                </div>
-
-                <p class="text-grey" v-if="service.description">
-                  {{ slice(service.description) }}
-                </p>
-
-                <!-- <div class="grid grid-cols-6 float-right">
-                <div
-                  class="col-span-1 -mr-12 "
-                  v-for="(stakeH, i) in service"
-                  :key="i"
-                >
-                  <img :src="stakeH.userPhoto" :alt="meta" />
-                </div>
-                <div
-                  class="col-span-1 -mr-12 bg-transparent rounded-full h-24 w-24 text-black text-xs font-medium"
-                >
-                  +{{ service.stakeholders.length }}
-                </div>
-              </div> -->
-              </div>
-            </nuxt-link>
+            <service-card :service="service" />
           </div>
-          htt
         </div>
 
         <div class="h-[70vh] flex flex-col items-center justify-center" v-else>
@@ -157,47 +86,7 @@
           v-if="stakeholderServices.length > 0"
         >
           <div v-for="service in stakeholderServices" :key="service.id">
-            <nuxt-link :to="`/services/${service.id}`">
-              <div class="card">
-                <div class="flex gap-x-[18px] items-center mb-24">
-                  <span
-                    class="rounded-[50%] h-[46px] w-[46px] leading-[46px] bg-[#D8DDFD] text-blue font-bold text-center"
-                  >
-                    {{ service.title.charAt(0).toUpperCase() }}
-                  </span>
-
-                  <h5 class="text-lg text-black font-medium truncate-lg">
-                    {{ service.title }}
-                  </h5>
-                </div>
-
-                <p
-                  class="text-base text-grey truncate-sm"
-                  v-if="service.description"
-                >
-                  {{ slice(service.description) }}
-                </p>
-
-                <p class="text-base text-grey" v-else>
-                  This service has no description.
-                </p>
-
-                <!-- <div class="grid grid-cols-6 float-right">
-                <div
-                  class="col-span-1 -mr-12 "
-                  v-for="(stakeH, i) in service"
-                  :key="i"
-                >
-                  <img :src="stakeH.userPhoto" :alt="meta" />
-                </div>
-                <div
-                  class="col-span-1 -mr-12 bg-transparent rounded-full h-24 w-24 text-black text-xs font-medium"
-                >
-                  +{{ service.stakeholders.length }}
-                </div>
-              </div> -->
-              </div>
-            </nuxt-link>
+            <service-card :service="service" />
           </div>
         </div>
 
@@ -216,11 +105,12 @@
 
 <script>
 import LgBtn from '~/components/LgBtn.vue'
+import ServiceCard from '~/components/services/ServiceCard.vue'
 import SvgLoader from '~/components/utils/SvgLoader.vue'
 import Loader from '~/components/utils/Loader.vue'
 export default {
   layout: 'dashboard',
-  components: { LgBtn, SvgLoader, Loader },
+  components: { ServiceCard, LgBtn, SvgLoader, Loader },
 
   data: () => ({
     merchantServices: [],
@@ -268,11 +158,6 @@ export default {
         console.log({ error })
       }
     },
-
-    slice(str) {
-      if (str.length <= 100) return str
-      return str.slice(0, 97) + '...'
-    },
   },
 
   mounted() {
@@ -280,27 +165,3 @@ export default {
   },
 }
 </script>
-
-<style lang="postcss" scoped>
-.card {
-  @apply bg-white p-20 pb-28 rounded-md shadow-[0_4px_50px_rgba(0,0,0,0.1)];
-}
-
-truncate {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  resize: horizontal;
-}
-
-/* truncate-lg {
-  @apply text-lg
-width: 200px;
-  font-size: 1.3rem;
-} */
-truncate-sm {
-  @apply text-base;
-  width: 200px;
-  font-size: 1.3rem;
-}
-</style>
