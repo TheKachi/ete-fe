bd1dd2746d3dc87caeb473f0d08df780bb4fe416
Onyedikachi Nze-Ben <nzebenflorence@gmail.com>
2022-04-28 22:00:24 +0100

    click btn scoped class delete

--- components/ClickBtn.vue
+++ components/ClickBtn.vue
@@ -9,9 +9,3 @@ export default {
   name: 'SubmitBtn',
 }
 </script>
-
-<style lang="postcss">
-.click-btn {
-  @apply bg-purple hover:bg-white text-white hover:text-purple border border-purple hover:border-purple rounded text-sm lg:text-base px-16 lg:px-36 py-8;
-}
-</style>
